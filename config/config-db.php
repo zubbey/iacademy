14e6f7b80d68bb5ffe7d9d8246763097fae61a68
zubbey <com.zubbey@hotmail.com>
2020-03-06 16:08:33 +0100

    modified db host

--- config/config-db.php
+++ config/config-db.php
@@ -15,7 +15,7 @@ if ($WEBSITE_ENVIRONMENT =="Development") {
     define("APP_BASE_URL", "http://localhost/iacademy");
     error_reporting(E_ALL ^ E_NOTICE); // turn ON showing errors
 } else {
-    $host 		= "us-cdbr-iron-east-05.cleardb.net";
+    $host 		= "us-cdbr-iron-east-04.cleardb.net";
     $user 		= "b69b0fcb927582";
     $password 	= "b00402f5";
     $database 	= "heroku_6ec76e33d788966";
