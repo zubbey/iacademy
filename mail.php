8a27c2dc3e98c6399ffe32af5376c7eb043d3bd8
zubbey <com.zubbey@hotmail.com>
2020-03-11 23:29:26 +0100

    modified mail messages 2

--- mail.php
+++ mail.php
@@ -1,15 +1,15 @@
 <?php
 function autoReply($email, $fname, $lname, $course){
-    $to = 'registration@ibrandafrica.one';
+    $to = 'registration@ibrandacademy.one, '. $email;
     $subject = 'iBrand Academy Enrollment System';
-    $from = $email;
+    $from = 'no-reply@ibrandacademy.one';
 
 // To send HTML mail, the Content-type header must be set
     $headers  = 'MIME-Version: 1.0' . "\r\n";
     $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
 
 // Create email headers
-    $admin = "com.zubbey@hotmail.com";
+    $admin = "com.zubbey@hotmail.com, ernest.nnadi@gmail.com, info@ibrandacademy.one";
     $headers .= 'From: '.$from."\r\n".
         'Reply-To: '.$from."\r\n" .
         'CC: '.$admin."\r\n" .
@@ -20,7 +20,7 @@ function autoReply($email, $fname, $lname, $course){
     $message .= '<div style="text-align: center; width: 100%; margin: auto; padding: 15px">';
     $message .= '<img id="logo" style="" src="ibrand.one/iacademy/img/iacademy_logo.png" width="127.76" height="48.73" alt="iAcademy logo">';
     $message .= '</div>';
-    $message .= '<h2>Hello '.$fname.' '.$lname.', </h2>';
+    $message .= '<h2 style="text-align: center">Hey '.$fname.' '.$lname.', </h2>';
     $message .= '<p>Congratulation on your registration, We are happy to guide you on the journey of '.$course.' <br>For more info kindly send us a mail at registration@ibrandacademy.one or call our hotline. <br><span style="color: #00aabd; font-size: small; font-style: italic;">+234(080) 212-60000
 | +234(080) 374-78593</span></p>
 ';
