d1f6033ba4106f0e4313927d5acfb3c482889b59
zubbey <56047720+zubbey@users.noreply.github.com>
2020-07-06 16:19:31 +0100

    Update Paystack API
    updated paystack key from Kingdom life (an expensive mistake) to ibrandafrica

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 var config = {
     GOOGLE_KEY : 'AIzaSyD2EAYEm2s0Y47rOb77dNPwdh6YhKYbUVo',
     PAYSTACK_SECRET_KEY : 'sk_live_a8e9cae80da01711f33cd2388cbcfc10b5002263',
-    PAYSTACK_PUBLIC_KEY : 'pk_live_3f38bcf11ebcc575162fe2e177cea7b114f2c5db'
+    PAYSTACK_PUBLIC_KEY : 'pk_live_6dd98d765b992e7f7b8f1cdb3efe9df157daef43'
 };
